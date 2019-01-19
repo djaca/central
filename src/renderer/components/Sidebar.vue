f5d2df938a8c199b86ce8fe74738003b0468651b
Aleksandar Savanovic <djaca85@gmail.com>
2019-01-19 20:22:27 +0100

    Add nav menu to sidebar

---
+++ src/renderer/components/Sidebar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="flex-none w-full md:max-w-xs bg-grey">
+    <div class="m-4">
+      <v-clock></v-clock>
+    </div>
+
+    <div class="menu mt-6">
+      <ul class="list-reset">
+        <li>
+          <router-link
+            exact
+            to="/"
+            class="block p-4 text-grey-darker font-bold hover:text-grey-dark"
+            active-class="border-grey-dark border-r-4"
+          >Pomodoro
+          </router-link>
+        </li>
+        <li>
+          <router-link
+            to="/morning-pages"
+            class="block p-4 text-grey-darker font-bold hover:text-grey-dark"
+            active-class="border-grey-dark border-r-4"
+          >Morning Pages
+          </router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+  import VClock from '@/components/VClock'
+
+  export default {
+    name: 'Sidebar',
+
+    components: {VClock}
+  }
+</script>
+
+<style scoped>
+  .menu a {
+    text-decoration: none;
+  }
+</style>
