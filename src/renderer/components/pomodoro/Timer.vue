38e74f0598f88462d171f364a01c376851a9c07f
Aleksandar Savanovic <djaca85@gmail.com>
2019-01-20 12:19:10 +0100

    Add break dialog

--- src/renderer/components/pomodoro/Timer.vue
+++ src/renderer/components/pomodoro/Timer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="p-2">
+  <div class="py-2 px-6">
     <div class="text-center">
       <div>
         <time class="text-green-dark">
@@ -34,6 +34,12 @@
       ...mapGetters('pomodoro', ['minutes', 'seconds', 'isActive', 'workSession'])
     },
 
+    watch: {
+      workSession () {
+        this.openBreakDialog()
+      }
+    },
+
     methods: {
       action () {
         this.isActive ? this.forfeit() : this.start()
@@ -50,6 +56,25 @@
           showCancelButton: true,
           confirmButtonText: 'Yes, I give up!',
           cancelButtonText: 'No! I can do this!'
+        }).then((result) => {
+          console.log('here')
+          if (result.value) {
+            this.$store.dispatch('pomodoro/forfeit')
+          }
+        })
+      },
+
+      openBreakDialog () {
+        this.$swal.fire({
+          title: 'You can now take 5 minutes break',
+          type: 'success',
+          allowEnterKey: false,
+          allowEscapeKey: false,
+          allowOutsideClick: false,
+          showCancelButton: true,
+          confirmButtonText: 'Skip break',
+          cancelButtonText: 'Take a break',
+          cancelButtonColor: 'blue'
         }).then((result) => {
           if (result.value) {
             this.$store.dispatch('pomodoro/forfeit')
