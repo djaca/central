97ec956c47f21f43a7eb9048858249ecc67d2d11
Aleksandar Savanovic <djaca85@gmail.com>
2019-02-07 09:09:53 +0100

    Add dummy icon and can minimize to tray

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,12 @@
 'use strict'
 
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow, Tray } from 'electron'
+
+const path = require('path')
+
+// const assetsDirectory = path.join(__dirname, '../renderer/assets')
+
+let tray = null
 
 /**
  * Set `__static` path to static files in production
@@ -32,7 +38,36 @@ function createWindow () {
   })
 }
 
-app.on('ready', createWindow)
+function createTray () {
+  tray = new Tray(path.join(__static, '/trayIcon.png'))
+  // tray.setToolTip('Central')
+
+  // const contextMenu = Menu.buildFromTemplate([
+  //   { label: 'Quit', click: () => { app.quit() } }
+  // ])
+  //
+  // tray.setContextMenu(contextMenu)
+
+  tray.on('click', event => {
+    toggleWindow()
+  })
+
+  // tray.on('right-click', toggleWindow)
+  // tray.on('double-click', toggleWindow)
+  // tray.on('click', function (event) {
+  //   toggleWindow()
+  //
+  //   // Show devtools when command clicked
+  //   if (window.isVisible() && process.defaultApp && event.metaKey) {
+  //     window.openDevTools({mode: 'detach'})
+  //   }
+  // })
+}
+
+app.on('ready', () => {
+  createTray()
+  createWindow()
+})
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -45,3 +80,7 @@ app.on('activate', () => {
     createWindow()
   }
 })
+
+const toggleWindow = () => {
+  mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show()
+}
